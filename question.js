158b342df86cd9002eb7afc0555db699f9bf8583
ndongwoo <ndongwoo@gmail.com>
2024-06-28 11:44:47 +0900

    Update question.js

--- question.js
+++ question.js
@@ -68,4 +68,31 @@ function calculateAge(event) {
 
     let surveyName = '';
     if (diffDays >= 2 && diffDays <= 12) surveyName = 'newborn';
-    else if (diffDays >= 13 && diffDays <=
+    else if (diffDays >= 13 && diffDays <= 30) surveyName = '2weeks';
+    else if (diffDays >= 31 && diffDays <= 121) surveyName = '2months';
+    else if (diffDays >= 122 && diffDays <= 182) surveyName = '4months';
+    else if (diffDays >= 183 && diffDays <= 273) surveyName = '6months';
+    else if (diffDays >= 274 && diffDays <= 364) surveyName = '9months';
+    else if (diffDays >= 365 && diffDays <= 455) surveyName = '12months';
+    else if (diffDays >= 456 && diffDays <= 546) surveyName = '15months';
+    else if (diffDays >= 547 && diffDays <= 683) surveyName = '18months';
+    else if (diffDays >= 684 && diffDays <= 913) surveyName = '24months';
+    else if (diffDays >= 914 && diffDays <= 1043) surveyName = '30months';
+    else if (diffDays >= 1044 && diffDays <= 1443) surveyName = '3years';
+    else if (diffDays >= 1444 && diffDays <= 1763) surveyName = '4years';
+    else if (diffDays >= 1764 && diffDays <= 1983) surveyName = '5years';
+    else if (diffDays >= 1984 && diffDays <= 2519) surveyName = '6years';
+    else if (diffDays >= 2520 && diffDays <= 3959) surveyName = '7-10years';
+    else if (diffDays >= 3960 && diffDays <= 4319) surveyName = '11years';
+    else if (diffDays >= 4320) surveyName = '12+years';
+
+    if (surveyName) {
+        window.location.href = `./survey.html?name=${surveyName}`;
+    } else {
+        alert('Invalid date range.');
+    }
+}
+
+window.onload = function() {
+    document.getElementById('visitDate').valueAsDate = new Date();
+};
