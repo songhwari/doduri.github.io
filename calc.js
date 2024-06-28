b20d37a8ff142a7518d88d27e440e63f04c102b8
ndongwoo <ndongwoo@gmail.com>
2024-06-28 14:40:51 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -66,9 +66,10 @@ function calculateFuWeight() {
 
     const diffTime = Math.abs(visitDate - lastVisitDate);
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+	const diffWeight = visitWeight - lastVisitWeight;
 
     const weightChangePerDay = (visitWeight - lastVisitWeight) / diffDays;
-    document.getElementById('result').innerHTML = `<strong>Daily Weight Change: ${weightChangePerDay.toFixed(2)} grams/day</strong>`;
+    document.getElementById('result').innerHTML = `<strong>Total Weigth Change: {$diffWeight} g</strong><br><strong>Duration: ${diffDays} days</strong><br><strong>Daily Weight Change: ${weightChangePerDay.toFixed(2)} grams/day</strong>`;
 }
 
 window.onload = initializeForm;
