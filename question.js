aa736a1a4020eafacb7a81fd10e71513fdf3e009
ndongwoo <ndongwoo@gmail.com>
2024-07-03 00:06:39 +0900

    update question

--- question.js
+++ question.js
@@ -5,6 +5,8 @@ function showOptions(optionType) {
     document.getElementById('pedsWellCheckUp').style.display = optionType === 'peds_well_check_up' ? 'block' : 'none';
     document.getElementById('hospAddress').style.display = optionType === 'hosp_address' ? 'block' : 'none';
     document.getElementById('bristolOptions').style.display = optionType === 'bristol' ? 'block' : 'none';
+    document.getElementById('tylenolImage').style.display = optionType === 'tylenol' ? 'block' : 'none';
+    document.getElementById('ibuprofenImage').style.display = optionType === 'ibuprofen' ? 'block' : 'none';
 }
 
 function showPedsWellCheckUp() {
@@ -65,6 +67,8 @@ function goBack() {
     document.getElementById('pedsWellCheckUp').style.display = 'none';
     document.getElementById('hospAddress').style.display = 'none';
     document.getElementById('bristolOptions').style.display = 'none';
+    document.getElementById('tylenolImage').style.display = 'none';
+    document.getElementById('ibuprofenImage').style.display = 'none';
 }
 
 function calculateAge(event) {
