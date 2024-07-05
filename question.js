e523cfdb098ee463a4d7eb2f26e2fba840fc474f
ndongwoo <ndongwoo@gmail.com>
2024-07-05 23:41:46 +0900

    update peds well checkup

--- question.js
+++ question.js
@@ -1,3 +1,24 @@
+const diffDaysList = [{label:'Newborn', name:'newborn',years:0,months:0,days:2}, 
+	{label:'2 weeks old', name:'2weeks',years:0,months:0,days:13},
+	{label:'2 months old', name:'2months',years:0,months:1,days:0},
+	{label:'4 months old', name:'4months',years:0,months:4,days:0},
+	{label:'6 months old', name:'6months',years:0,months:6,days:0},
+	{label:'9 months old', name:'9months',years:0,months:9,days:0},
+	{label:'12 months old', name:'12months',years:1,months:0,days:0},
+	{label:'15 months old', name:'15months',years:1,months:3,days:0},
+	{label:'18 months old', name:'18months',years:1,months:6,days:0},
+	{label:'24 months old', name:'24months',years:1,months:11,days:0},
+	{label:'30 months old', name:'30months',years:2,months:5,days:0},
+	{label:'3 years old', name:'3years',years:2,months:11,days:0},
+	{label:'4 years old', name:'4years',years:3,months:11,days:0},
+	{label:'5 years old', name:'5years',years:4,months:11,days:0},
+	{label:'6 years old', name:'6years',years:5,months:6,days:0},
+	{label:'7 years old', name:'7years',years:7,months:0,days:0},
+	{label:'8-10 years old', name:'8-10years',years:8,months:0,days:0},
+	{label:'11 years old', name:'11years',years:11,months:0,days:0},
+	{label:'12-18 years old', name:'12-18years',years:12,months:0,days:0},
+	{label:'Adult', name:'Adult',years:19,months:0,days:0}];
+
 function showOptions(optionType) {
     document.getElementById('initialOptions').style.display = 'none';
     document.getElementById('adultOptions').style.display = optionType === 'adult' ? 'block' : 'none';
@@ -14,30 +35,6 @@ function showOptions(optionType) {
     document.getElementById('fuWeightForm').style.display = optionType === 'fu_weight' ? 'block' : 'none';
     document.getElementById('rankImage').style.display = optionType === 'rank' ? 'block' : 'none';
     document.getElementById('pedsvitalsignImage').style.display = optionType === 'pedsvitalsign' ? 'block' : 'none';
-}
-
-function populateDob() {
-    const dobYearSelect = document.getElementById('dobYear');
-    const currentYear = new Date().getFullYear();
-    const startYear = currentYear - 18;
-
-    for (let year = currentYear; year >= startYear; year--) {
-        const option = document.createElement('option');
-        option.value = year;
-        option.textContent = year;
-        dobYearSelect.appendChild(option);
-    }
-
-    const dobDaySelect = document.getElementById('dobDay');
-    if (dobDaySelect.options.length > 0) {
-        return;
-    }
-	for (let day = 1; day <= 31; day++) {
-        const option = document.createElement('option');
-        option.value = day;
-        option.textContent = day;
-        dobDaySelect.appendChild(option);
-    }
 }
 
 function populateLastVisitDate() {
@@ -65,9 +62,6 @@ function setTodayAsDefault() {
     let today = new Date();
 
     document.getElementById('visitDate').value = today.toISOString().slice(0,10);
-    document.getElementById('dobYear').value = today.getFullYear();
-    document.getElementById('dobMonth').value = today.getMonth() + 1; // Months are 0-indexed
-    document.getElementById('dobDay').value = today.getDate();
 
     document.getElementById('visitDate2').value = today.toISOString().slice(0,10);
     document.getElementById('dobYear2').value = today.getFullYear();
@@ -97,41 +91,43 @@ function goBack() {
     document.getElementById('pedsvitalsignImage').style.display = 'none';
 }
 
-function calculateAge(event) {
-    event.preventDefault();
-    const dobYear = parseInt(document.getElementById('dobYear').value);
-    const dobMonth = parseInt(document.getElementById('dobMonth').value) - 1; // Months are 0-indexed
-    const dobDay = parseInt(document.getElementById('dobDay').value);
-    const visitDate = new Date(document.getElementById('visitDate').value);
-    const dob = new Date(dobYear, dobMonth, dobDay);
-    const diffTime = Math.abs(visitDate - dob);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-
-    let surveyName = '';
-    if (diffDays >= 2 && diffDays <= 12) surveyName = 'newborn';
-    else if (diffDays >= 13 && diffDays <= 30) surveyName = '2weeks';
-    else if (diffDays >= 31 && diffDays <= 121) surveyName = '2months';
-    else if (diffDays >= 122 && diffDays <= 182) surveyName = '4months';
-    else if (diffDays >= 183 && diffDays <= 273) surveyName = '6months';
-    else if (diffDays >= 274 && diffDays <= 364) surveyName = '9months';
-    else if (diffDays >= 365 && diffDays <= 455) surveyName = '12months';
-    else if (diffDays >= 456 && diffDays <= 546) surveyName = '15months';
-    else if (diffDays >= 547 && diffDays <= 683) surveyName = '18months';
-    else if (diffDays >= 684 && diffDays <= 913) surveyName = '24months';
-    else if (diffDays >= 914 && diffDays <= 1043) surveyName = '30months';
-    else if (diffDays >= 1044 && diffDays <= 1443) surveyName = '3years';
-    else if (diffDays >= 1444 && diffDays <= 1763) surveyName = '4years';
-    else if (diffDays >= 1764 && diffDays <= 1983) surveyName = '5years';
-    else if (diffDays >= 1984 && diffDays <= 2519) surveyName = '6years';
-    else if (diffDays >= 2520 && diffDays <= 3959) surveyName = '7-10years';
-    else if (diffDays >= 3960 && diffDays <= 4319) surveyName = '11years';
-    else if (diffDays >= 4320) surveyName = '12+years';
-
-    if (surveyName) {
-        window.location.href = `./screening.htm?name=${surveyName}`;
-    } else {
-        alert('Invalid date range.');
+function calculatePreviousDate(endDate, diff) {
+    const end = new Date(endDate);
+    
+    let yearDiff = diff.years;
+    let monthDiff = diff.months;
+    let dayDiff = diff.days;
+    
+    // Subtract the year difference
+    let previousYear = end.getFullYear() - yearDiff;
+    
+    // Subtract the month difference
+    let previousMonth = end.getMonth() - monthDiff;
+    if (previousMonth < 0) {
+        previousYear -= 1;
+        previousMonth += 12;
     }
+    
+    // Calculate the target day in the previous month
+    let previousDate = new Date(previousYear, previousMonth, end.getDate());
+    
+    // Check if the calculated day is valid
+    if (previousDate.getMonth() !== previousMonth) {
+        // Use the last day of the previous month if the date is invalid
+        previousDate = new Date(previousYear, previousMonth + 1, 0);
+    }
+    
+    // Adjust the day difference
+    if (dayDiff > 0) {
+        previousDate.setDate(previousDate.getDate() - dayDiff);
+    }
+
+    // Convert the date to YYYY-MM-DD format
+    const year = previousDate.getFullYear();
+    const month = (previousDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = previousDate.getDate().toString().padStart(2, '0');
+    
+    return `${year}-${month}-${day}`;
 }
 
 function calculateWeightLoss() {
@@ -169,8 +165,31 @@ function calculateFuWeight() {
 	}
 }
 
+function calculateRanges() {
+	const endDate = document.getElementById('visitDate').value;
+	
+	const buttonsContainer = document.getElementById('buttonsContainer');
+	buttonsContainer.innerHTML = '';
+
+	diffDaysList.forEach((diff, index) => {
+		if (index == diffDaysList.length-1) return;//last=Adult
+
+		const startDate = calculatePreviousDate(endDate, diff);
+		const endDateForRange = calculatePreviousDate(calculatePreviousDate(endDate, diffDaysList[index+1]), {years: 0, months: 0, days: 1});
+		
+		const button = document.createElement('button');
+		button.textContent = `${diff.label}: ${endDateForRange} ~ ${startDate}`;
+		button.className = 'btn btn-secondary btn-block mb-2';
+		button.onclick = function() {
+			window.location.href = `./screening.htm?name=${diff.name}`;
+        };
+		buttonsContainer.appendChild(button);
+	});
+}
+
 window.onload = function() {
-    populateDob();
 	populateLastVisitDate();
     setTodayAsDefault();
+
+	calculateRanges();
 };
